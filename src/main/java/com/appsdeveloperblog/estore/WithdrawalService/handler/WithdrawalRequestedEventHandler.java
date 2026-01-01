2538b1adf661fdbabdbb7522e51b26e3c0d64b6f
volodya4u <volodya4u@hotmail.com>
2026-01-02 01:37:42 +0200

    Add lombok dependency

--- src/main/java/com/appsdeveloperblog/estore/WithdrawalService/handler/WithdrawalRequestedEventHandler.java
+++ src/main/java/com/appsdeveloperblog/estore/WithdrawalService/handler/WithdrawalRequestedEventHandler.java
@@ -1,21 +1,20 @@
 package com.appsdeveloperblog.estore.WithdrawalService.handler;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
-import com.appsdeveloperblog.payments.ws.core.events.WithdrawalRequestedEvent;
+import com.appsdeveloperblog.ws.core.events.WithdrawalRequestedEvent;
 
+@Slf4j
 @Component
 @KafkaListener(topics = "withdraw-money-topic", containerFactory = "kafkaListenerContainerFactory")
 public class WithdrawalRequestedEventHandler {
-    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
     @KafkaHandler
     public void handle(@Payload WithdrawalRequestedEvent withdrawalRequestedEvent) {
-        LOGGER.info("Received a new withdrawal event: {} ", withdrawalRequestedEvent.getAmount());
+        log.info("Received a new withdrawal event: {} ", withdrawalRequestedEvent.getAmount());
     }
 }
